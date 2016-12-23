c26e1d959e90bc87c6cdcea6c7059d302c6c633f
Kate F <kate@elide.org>
2016-12-23 16:35:47 +0000

    Oops... this should have been committed in [169].

--- src/rrd/pretty.h
+++ src/rrd/pretty.h
@@ -9,5 +9,6 @@ void rrd_pretty_prefixes(struct node **);
 void rrd_pretty_suffixes(struct node **);
 void rrd_pretty_bottom(struct node **);
 void rrd_pretty_redundant(struct node **);
+void rrd_pretty_collapse(struct node **);
 
 #endif
